78eb8c57540e32a4553f62e9b11de9c61da9ed27
TimMannn <verbakel.t@gmail.com>
2025-06-17 11:05:44 +0200

    Mogelijke pipeline error fix

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react'
 import fs from 'fs'
 import path from 'path'
 
+// __dirname simuleren in ES modules:
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   plugins: [react()],
   server: {
